32c5b9edef320adf3a2c94b25ac04c94909f6469
fabiobrasileiroo <fabio.h591@gmail.com>
2023-09-08 19:26:04 -0400

    atividade

---
+++ logica/01.py
@@ -0,0 +1,14 @@
+"""nota1 = float(input("Escreva sua nota: "))
+nota2 = float(input("Escreva sua segunda nota: "))
+
+media = (nota1 + nota2) / 2
+if(media >= 7):
+    print("A média das suas notas é", media)
+else:
+    print("reprovado")"""
+a = 3,3
+print (a)
+
+feriado = False
+if !(feriado == true):
+    print('funcionar')
