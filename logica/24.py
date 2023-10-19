a2513e2cadbaf3f4c271a8f7f2c75431f463f7db
fabiobrasileiroo <fabio.h591@gmail.com>
2023-10-19 11:42:35 -0400

    atividade feita

---
+++ logica/24.py
@@ -0,0 +1,14 @@
+def acharElemento(elem, arr):
+    achou = False
+    cumprimento = len(arr)
+
+    for i in range(cumprimento):
+        if (arr[i] == elem):
+            achou = True
+    if(achou == True):
+        print("Achei um nome: " + elem)
+    else:
+        print("Não achei o nome: " + elem)
+
+nomes = ["Roberto","Joao","Fabio","Jo"]
+acharElemento("Roberto", nomes)
