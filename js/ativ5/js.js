6be3b4dc2e48257362e48d2457c1532a9edec540
fabiopc <fabiopc@debian-BULLSEYE-live-builder-AMD64>
2023-11-10 19:52:25 -0400

    ativ tabuada

---
+++ js/ativ5/js.js
@@ -0,0 +1,8 @@
+function tabuada(num1, num2) {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+    console.log(`${num1} x ${num2} = ${num1 * num2}`);
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+}
+
+tabuada(4, 5);
