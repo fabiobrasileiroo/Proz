e95a452f7d3a633f54de3f2ef3b2494a12ad6774
fabiopc <fabiopc@debian-BULLSEYE-live-builder-AMD64>
2023-11-14 16:58:23 -0400

    treinando flex-box animations

--- js/ativ5/js.js
+++ js/ativ5/js.js
@@ -2,7 +2,7 @@ function tabuada(num1, num2) {
     console.log(`${num1} + ${num2} = ${num1 + num2}`);
     console.log(`${num1} - ${num2} = ${num1 - num2}`);
     console.log(`${num1} x ${num2} = ${num1 * num2}`);
-    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+    console.log(`${num1} / ${num2} = ${(num1 / num2).toFixed(2)}`);
 }
 
 tabuada(4, 5);
