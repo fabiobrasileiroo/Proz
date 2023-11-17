c5350139e575ac7f1d5f259b29c32672a087869d
fabiobrasileiroo <fabio.h591@gmail.com>
2023-11-17 08:32:05 -0400

    estudo

---
+++ js/js/script.js
@@ -0,0 +1,7 @@
+let elementoJavacript = document.createElement("li");
+
+elementoJavacript.innerText = "JavaScript"
+elementoJavacript.id = "ling-js"
+
+let listaLinguagens = document.querySelector(".lista-linguagens")
+listaLinguagens.appendChild(elementoJavacript)
