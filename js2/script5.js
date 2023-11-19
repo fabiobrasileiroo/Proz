c341d7787c943b313ba658134236acb07bfd8a51
fabiopc <fabiopc@debian-BULLSEYE-live-builder-AMD64>
2023-11-19 08:35:20 -0400

    addEnventListener

---
+++ js2/script5.js
@@ -0,0 +1,9 @@
+let elementoClicado = document.getElementById("elemento-clicado")
+let botaoTempo = document.getElementById("botao-tempo")
+let tempo = document.getElementById("tempo")
+
+botaoTempo.addEventListener("click", (e)=> {
+    let tempoNormal = (e.timeStamp)/1000
+    console.log(tempoNormal)
+    tempo.innerText = tempoNormal + "s" 
+})
