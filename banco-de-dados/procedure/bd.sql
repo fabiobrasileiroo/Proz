49635fc4351569c3b5f0519cc14b7a96a934c2e5
fabiobrasileiroo <fabio.h591@gmail.com>
2023-12-26 16:35:33 -0400

    ativ

--- banco-de-dados/procedure/bd.sql
+++ banco-de-dados/procedure/bd.sql
@@ -1,3 +1,20 @@
+-- Criar a tabela de exemplo
+CREATE TABLE vendas (
+    id SERIAL PRIMARY KEY,
+    data_compra DATE,
+    quantidade INT
+);
+
+-- Inserir alguns dados fictícios
+INSERT INTO vendas (data_compra, quantidade) VALUES
+    ('2023-12-26', 10),
+    ('2023-12-26', 5),
+    ('2023-12-27', 8),
+    ('2023-12-27', 12),
+    ('2023-12-27', 6),
+    ('2023-12-28', 15);
+
+-- Criar a stored procedure
 CREATE OR REPLACE FUNCTION LevantamentoDiario()
 RETURNS TABLE (Dia DATE, QuantidadeTotal INT)
 AS $$
@@ -12,3 +29,5 @@ BEGIN
         data_compra::DATE;
 END;
 $$ LANGUAGE plpgsql;
+
+SELECT * FROM LevantamentoDiario();
