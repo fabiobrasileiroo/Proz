5f294483fbd37224cafc9dd9615149483463a9b6
fabiobrasileiroo <fabio.h591@gmail.com>
2024-01-02 10:08:53 -0400

    teste

--- api/fetch-api/script.js
+++ api/fetch-api/script.js
@@ -1,3 +1,4 @@
+// pegando tags no html
 const btnUsuario = document.getElementById('btn-usuario')
 const  usuariosContainer = document.getElementById('usuarios-container')
 const helperTextUsuario  = document.getElementById('helper-text-usuario')
@@ -16,6 +17,7 @@ function gerarUsuario() {
         console.log(data)
     })
 */
+    helperTextUsuario.innerHTML = 'Carregando...'
     fetch('https://random-data-api.com/api/v2/users')
         .then((res) => res.json())
         .then((data) => {
@@ -24,9 +26,68 @@ function gerarUsuario() {
             <img src="${data.avatar}"/> 
             <span><strong>Username:</strong>${data.username}</span>
             `
+            usuario.classList.add('usuario')
             usuariosContainer.appendChild(usuario)
+            setInterval(() => {
+                helperTextUsuario.innerHTML = ''
+            },1000)
             console.log(data)
         })
+        .catch((error)=> {
+            helperTextUsuario.innerHTML = 'Não foi possivel gerar um usuário'
+            console.log(error)
+        })
+}
+// Eventos de click
+btnUsuario .addEventListener('click', gerarUsuario)
+
+// Gerador de postagens
+// Captura de elementos
+const postTitle = document.getElementById('post-title')
+const postBody = document.getElementById('post-body')
+const btnPost = document.getElementById('btn-post')
+const postsContainer = document.getElementById('posts-container')
+const helperTextPost = document.getElementById('helper-text-post')
+
+function gerarPost(evento) { 
+    evento.preventDefault()
+
+    console.log(postTitle.value)
+    console.log(postBody.value)
+
+    const jsonBody = JSON.stringify({
+        titulo:postTitle.value,
+        mensagem:postBody.value
+    })
+
+    console.log(jsonBody)
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'Post',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: jsonBody
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        const post = document.createElement('div')
+        post.classList.add('postagem')
+        post.innerHTML = `
+            <h3>${data.id} - ${data.titulo}</h3> 
+            <p>${data.mensagem}</p>
+        `
+        postsContainer.prepend(post)
+        // Limpar o formulario
+        postTitle.value= ''
+        postBody.value = ''
+        alert("adicionado com sucesso")
+        })
+    .catch((error) => {
+        console.log(error)
+        helperTextPost.innerHTML = 'Não foi possivel gerar a postagem '
+    })
 }
 
-btnUsuario .addEventListener('click', gerarUsuario)
+//3. Eventos
+btnPost.addEventListener('click',(evento) => gerarPost(evento))
